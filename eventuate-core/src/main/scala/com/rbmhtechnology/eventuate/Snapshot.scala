303121fc06c8c51741a3d26e71781df9f3ca11d0
coding4m <coding4m@gmail.com>
2016-11-19 21:16:39 +0800

    1. refactor.

--- eventuate-core/src/main/scala/com/rbmhtechnology/eventuate/Snapshot.scala
+++ eventuate-core/src/main/scala/com/rbmhtechnology/eventuate/Snapshot.scala
@@ -18,12 +18,13 @@ package com.rbmhtechnology.eventuate
 
 import com.rbmhtechnology.eventuate.ConfirmedDelivery.DeliveryAttempt
 import com.rbmhtechnology.eventuate.PersistOnEvent.PersistOnEventRequest
+import com.typesafe.config.Config
 
 /**
  * Snapshot metadata.
  *
- * @param emitterId Id of the [[EventsourcedActor]], [[EventsourcedView]], stateful [[EventsourcedWriter]]
- *                  or [[EventsourcedProcessor]] that saves the snapshot.
+ * @param emitterId  Id of the [[EventsourcedActor]], [[EventsourcedView]], stateful [[EventsourcedWriter]]
+ *                   or [[EventsourcedProcessor]] that saves the snapshot.
  * @param sequenceNr The highest event sequence number covered by the snapshot.
  */
 case class SnapshotMetadata(emitterId: String, sequenceNr: Long)
@@ -35,13 +36,13 @@ case class SnapshotMetadata(emitterId: String, sequenceNr: Long)
  * and [[EventsourcedProcessor]]s can save snapshots of internal state by calling the (inherited)
  * [[EventsourcedView#save]] method.
  *
- * @param payload Application-specific snapshot.
- * @param emitterId Id of the event-sourced actor, view, stateful writer or processor that saved the snapshot.
- * @param lastEvent Last handled event before the snapshot was saved.
- * @param currentTime Current vector time when the snapshot was saved.
- * @param sequenceNr Sequence number of the last *received* event when the snapshot was saved.
- * @param deliveryAttempts Unconfirmed [[ConfirmedDelivery.DeliveryAttempt DeliveryAttempt]]s when the snapshot was
- *                         saved (can only be non-empty if the actor implements [[ConfirmedDelivery]]).
+ * @param payload                Application-specific snapshot.
+ * @param emitterId              Id of the event-sourced actor, view, stateful writer or processor that saved the snapshot.
+ * @param lastEvent              Last handled event before the snapshot was saved.
+ * @param currentTime            Current vector time when the snapshot was saved.
+ * @param sequenceNr             Sequence number of the last *received* event when the snapshot was saved.
+ * @param deliveryAttempts       Unconfirmed [[ConfirmedDelivery.DeliveryAttempt DeliveryAttempt]]s when the snapshot was
+ *                               saved (can only be non-empty if the actor implements [[ConfirmedDelivery]]).
  * @param persistOnEventRequests Unconfirmed [[PersistOnEvent.PersistOnEventRequest PersistOnEventRequest]]s when the
  *                               snapshot was saved (can only be non-empty if the actor implements [[PersistOnEvent]]).
  */
@@ -52,7 +53,8 @@ case class Snapshot(
   currentTime: VectorTime,
   sequenceNr: Long,
   deliveryAttempts: Vector[DeliveryAttempt] = Vector.empty,
-  persistOnEventRequests: Vector[PersistOnEventRequest] = Vector.empty) {
+  persistOnEventRequests: Vector[PersistOnEventRequest] = Vector.empty
+) {
 
   val metadata: SnapshotMetadata =
     SnapshotMetadata(emitterId, sequenceNr)
@@ -62,4 +64,8 @@ case class Snapshot(
 
   def addPersistOnEventRequest(persistOnEventRequest: PersistOnEventRequest): Snapshot =
     copy(persistOnEventRequests = persistOnEventRequests :+ persistOnEventRequest)
+}
+
+class SnapshotSettings(config: Config) {
+  val interval = config.getInt("eventuate.snapshot.interval")
 }
---
+++ eventuate-core/src/main/scala/com/rbmhtechnology/eventuate/SnapshotOnEvent.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015 - 2016 Red Bull Media House GmbH <http://www.redbullmediahouse.com> - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rbmhtechnology.eventuate
+
+import akka.contrib.pattern.ReceivePipeline
+import akka.contrib.pattern.ReceivePipeline.Inner
+
+/**
+  * @author siuming
+  */
+trait SnapshotOnEvent {
+
+  this: EventsourcedView with ReceivePipeline =>
+
+  import EventsourcingProtocol._
+
+  private var snapshotOffset: Int = 0
+  private var snapshotInProgress: Boolean = false
+  private val snapshotSettings = new SnapshotSettings(context.system.settings.config)
+
+  def snapshotOrDelay() = {
+    val extraOffset = if (snapshotInProgress) snapshotSettings.interval else 0
+    if (snapshotOffset - extraOffset >= snapshotSettings.interval) {
+      self ! Snapshotting
+      snapshotInProgress = true
+    }
+  }
+
+  pipelineOuter {
+
+    case ss@SaveSnapshotSuccess =>
+      snapshotOffset = 0
+      snapshotInProgress = false
+      Inner(ss)
+
+    case rs@ReplaySuccess(events, _, _) =>
+      snapshotOffset += events.size
+      snapshotOrDelay()
+      Inner(rs)
+
+    case ws@WriteSuccess(events, _, _) =>
+      snapshotOffset += events.size
+      snapshotOrDelay()
+      Inner(ws)
+
+    // sent by an event log when replicate success
+    case w@Written =>
+      snapshotOffset += 1
+      snapshotOrDelay()
+      Inner(w)
+
+    case any =>
+      Inner(any)
+  }
+
+}
+
+
