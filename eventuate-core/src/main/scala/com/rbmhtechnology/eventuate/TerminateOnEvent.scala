4b1dbf9142abe673decb1e1339ef887daa684fc2
coding4m <coding4m@gmail.com>
2017-01-20 14:36:31 +0800

    1. refactor.

---
+++ eventuate-core/src/main/scala/com/rbmhtechnology/eventuate/TerminateOnEvent.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 - 2016 Red Bull Media House GmbH <http://www.redbullmediahouse.com> - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rbmhtechnology.eventuate
+
+import akka.actor.{ Actor, PoisonPill, ReceiveTimeout }
+import akka.contrib.pattern.ReceivePipeline
+import akka.contrib.pattern.ReceivePipeline.{ HandledCompletely, Inner }
+
+import scala.concurrent.duration._
+
+/**
+ * @author siuming
+ */
+trait TerminateOnEvent { this: EventsourcedView with ReceivePipeline =>
+  lazy val terminateSettings: TerminateSettings = TerminateSettings(PoisonPill, 30.minutes, context.parent)
+
+  context.setReceiveTimeout(terminateSettings.terminateTimeout)
+  pipelineOuter {
+    case ReceiveTimeout =>
+      terminateSettings.terminateSupervisor ! terminateSettings.terminateMsg
+      HandledCompletely
+    case any =>
+      Inner(any)
+  }
+
+}
